5e5bcfd51b90880a9c2014886f1776d1535093e1
Erick <erickcampaz@gmail.com>
2022-11-19 16:44:06 -0600

    Praticando libreria Bootstramp

---
+++ src/App.js
@@ -0,0 +1,28 @@
+/* npm install bootstrap@5.2.2 */
+
+import React from 'react';
+
+/* import './App.css'; */
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hola Mundo</h1>
+      <section>
+      <button type="button" class="btn btn-secondary">Secondary</button>
+      
+      <button type="button" class="btn btn-success">Success</button>
+   
+      <button type="button" class="btn btn-danger">Danger</button>
+ 
+      <button type="button" class="btn btn-warning">Warning</button>
+     
+      <button type="button" class="btn btn-info">Info</button>
+     
+      <button type="button" class="btn btn-light">Light</button>
+      </section>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Card.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+
+
+function Card() {
+    return (
+        <div class="card">
+        <div class="card-body">
+          This is some text within a card body.
+        </div>
+      </div>
+    );
+  }
+  
+  export default Card;
+  
